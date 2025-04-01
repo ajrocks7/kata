b0ac874feb9242d340237ad5747e6c0bed7700b5
ajrocks7 <amaltorres07@gmail.com>
2025-04-01 16:51:11 +0530

    kataOne-refactoring

--- tests/KataOneTest.php
+++ tests/KataOneTest.php
@@ -12,8 +12,7 @@ class KataOneTest extends TestCase
 {
     public function test_it_returns_zero_for_empty_string():void
     {
-        $stringCalculator = new KataOne();
-        $result = $stringCalculator->add("");
+        $result = $this->KataOne()->add("");
 
         assertThat($result,is(equalTo(0)));
         $this->addToAssertionCount(1);
@@ -21,8 +20,7 @@ class KataOneTest extends TestCase
 
     public function test_it_returns_the_sum_of_numbers_for_given_string():void
     {
-        $stringCalculator =  new KataOne();
-        $result = $stringCalculator->add("1,2");
+        $result = $this->KataOne()->add("1,2");
 
         assertThat($result,is(equalTo(3)));
         $this->addToAssertionCount(1);
@@ -30,10 +28,14 @@ class KataOneTest extends TestCase
 
     public function test_it_returns_the_sum_of_numbers_even_when_an_empty_string_is_included():void
     {
-        $stringCalculator =  new KataOne();
-        $result = $stringCalculator->add('1,2,"",4,5');
+        $result = $this->KataOne()->add('1,2,"",4,5');
 
         assertThat($result,is(equalTo(12)));
         $this->addToAssertionCount(1);
+    }
+
+    private function KataOne(): KataOne
+    {
+        return new KataOne();
     }
 }
